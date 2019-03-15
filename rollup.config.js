74015c3cac8377901b38306bd3582756ef351b3d
cdtinney <colintinney@gmail.com>
2019-03-15 17:56:58 +0800

    add esm builds to package

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,26 @@
 import babel from 'rollup-plugin-babel';
 
-module.exports = {
+module.exports = [{
   input: 'src/index.jsx',
   output: {
     file: 'dist/bundle.js',
     name: 'Marquee',
-    format: 'cjs',
+    format: 'umd',
   },
   plugins: [
     babel({
       exclude: 'node_modules/**',
     }),
   ],
-};
+}, {
+  input: 'src/index.jsx',
+  output: {
+    file: 'dist/bundle.esm.js',
+    format: 'esm',
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
+}];
