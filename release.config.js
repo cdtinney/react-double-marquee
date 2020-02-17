d30e0e29568766fd3c387afcc7c71d0b6e06646b
Colin Tinney <cdtinney@users.noreply.github.com>
2020-02-16 17:18:46 -0800

    chore: update release config

--- release.config.js
+++ release.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      preset: 'angular',
+      releaseRules: [{
+        type: 'docs',
+        release: 'patch',
+      }, {
+        type: 'chore',
+        release: 'patch',
+      },
+      {
+        type: 'refactor',
+        release: 'patch',
+      },
+      {
+        type: 'style',
+        release: 'patch',
+      },
+      ],
+    }],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     ['@semantic-release/npm', {
